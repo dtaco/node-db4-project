f8223d6750ddecda52ce9c1e335636b65ad00950
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-08 12:00:49 -0500

    recipes_model and MVP

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -18,7 +18,45 @@ async function getRecipeById(recipe_id) {
         .orderBy('s.step_number')
         .where('r.recipe_id', recipe_id)
 
-    return recipeRows;
+        const recipes = {
+            recipe_id: recipeRows[0].recipe_id,
+            recipe_name: recipeRows[0].recipe_name,
+            steps: recipeRows.reduce((acc, row) => {
+                if (!row.ingredient_id) {
+                    return acc.concat({
+                        step_id: row.step_id,
+                        step_number: row.step_number,
+                        step_instructions: row.step_text,
+                    })
+                }
+
+                if (row.ingredient_id && !acc.find(step => step.step_id === row.step_id)) {
+                        return acc.concat({
+                            step_id: row.step_id,
+                            step_number: row.step_number,
+                            step_instructions: row.step_text,
+                            ingredients: [
+                                {
+                                    ingredient_id: row.ingredient_id,
+                                    ingredient_name: row.ingredient_name,
+                                    quantity: row.quantity,
+                                    }
+                                ]
+                            })
+                        }
+                
+                const currentStep = acc.find(step => step.step_id === row.step_id)
+                currentStep.ingredients.push({
+                    ingredient_id: row.ingredient_id,
+                    ingredient_name: row.ingredient_name,
+                    quantity: row.quantity,
+                })
+                return acc
+            }, [])
+
+        }
+
+    return recipes;
 }
 
 module.exports = {
