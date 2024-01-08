b50495b54390e5877f73c5df65ca8c28bfcc352f
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-07 22:56:25 -0500

    fixed switched seed files

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -1,8 +1,4 @@
-const { clean } = require('knex-cleaner')
 
 exports.seed = function (knex) {
-    return clean(knex, {
-        mode: 'truncate',
-        ignoreTables: ['knex_migrations', 'knex_migrations_lock']
-    })
+    
 }
