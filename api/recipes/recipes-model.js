f268d3f4eba8a0903c706c9cd0e8854fc56f39c8
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-07 23:47:58 -0500

    getrecipebyid has been updated

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,7 +2,21 @@ const db = require('../../data/db-config')
 
 async function getRecipeById(recipe_id) {
     const recipeRows = await db('recipes as r')
-        .where('recipe_id', recipe_id)
+        .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+        .leftJoin('step_ingredients as si', 'si.step_id', 's.step_id')
+        .leftJoin('ingredients as i', 'i.ingredient_id', 'si.ingredient_id')
+        .select(
+            'r.recipe_id',
+            'r.recipe_name',
+            's.step_id',
+            's.step_number',
+            's.step_text',
+            'i.ingredient_id',
+            'i.ingredient_name',
+            'si.quantity',
+        )
+        .orderBy('s.step_number')
+        .where('r.recipe_id', recipe_id)
 
     return recipeRows;
 }
